01a1bbb88e30dacbe3dc3616da339200d1ecb683
Tim Honermann <honermti@windowslive.com>
2024-03-03 17:03:11 +0100

    chore: added tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SignalInputComponent } from './signal-input/signal-input.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  })
 });
--- src/app/signal-input/signal-input.component.spec.ts
+++ src/app/signal-input/signal-input.component.spec.ts
@@ -11,13 +11,27 @@ describe('SignalInputComponent', () => {
       imports: [SignalInputComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(SignalInputComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('count', 0);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should double count input', () => {
+    // arrange
+    const count = 4;
+    const expectedDoubledCount = 8;
+
+    // act
+    fixture.componentRef.setInput('count', count);
+
+    // assert
+    expect(component.doubleCount()).toEqual(expectedDoubledCount);
   });
 });
